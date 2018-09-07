58d9f0a3f0f579070144ee5845bd4fa1d6737d95
Laura Martin <laura.martin@futurelearn.com>
2018-09-07 18:32:14 +0100

    Add fixes as suggested by Rubocop

--- drone-slack.rb
+++ drone-slack.rb
@@ -18,19 +18,19 @@ class DroneSlack
           title_link: link,
           fields: [
             {
-              title: "Status",
+              title: 'Status',
               value: build_status[:message],
-              short: true,
+              short: true
             },
             {
-              title: "Committer",
+              title: 'Committer',
               value: author,
-              short: true,
+              short: true
             },
             {
-              title: "Commit",
+              title: 'Commit',
               value: commit_summary,
-              short: false,
+              short: false
             }
           ]
         }
@@ -40,31 +40,31 @@ class DroneSlack
 
   def notify
     if post_check
-      puts "Posting message to Slack"
+      puts 'Posting message to Slack'
       HTTParty.post(
         webhook,
-        body: template,
+        body: template
       ).body
     else
-      puts "Build in good state. Not posting Slack message."
+      puts 'Build in good state. Not posting Slack message.'
     end
   end
 
   # Produces a hash depending on the status of the build
   def build_status
-    if status == "success"
+    if status == 'success'
       {
-        color: "good",
+        color: 'good',
         message: ":tada: Succeeded (#{time_taken}) :tada:"
       }
     elsif status == 'success' && prev_build_status == 'failure'
       {
-        color: "good",
+        color: 'good',
         message: ":sweat_smile: Recovered (#{time_taken}) :nail_care:"
       }
     else
       {
-        color: "danger",
+        color: 'danger',
         message: ":crying_cat_face: Failed (#{time_taken}) :sadparrot:"
       }
     end
@@ -83,15 +83,11 @@ class DroneSlack
 
   def time_taken
     seconds = finished.to_i - started.to_i
-    if seconds < 60
-      return "#{seconds}s"
-    end
+    return "#{seconds}s" if seconds < 60
 
-    if seconds < 3600
-      return Time.at(seconds).utc.strftime("%Mm %Ss")
-    end
+    return Time.at(seconds).utc.strftime('%Mm %Ss') if seconds < 3600
 
-    Time.at(seconds).utc.strftime("%H:%M:%S")
+    Time.at(seconds).utc.strftime('%H:%M:%S')
   end
 
   def post_check
@@ -99,10 +95,8 @@ class DroneSlack
 
     if status == 'success' && prev_build_status == 'failure'
       true
-    elsif status == 'failure'
-      true
     else
-      false
+      status == 'failure'
     end
   end
 
@@ -111,11 +105,9 @@ class DroneSlack
   end
 
   def set_parameter(parameter_name, required = true)
-    parameter = "PLUGIN_" + parameter_name.upcase
+    parameter = 'PLUGIN_' + parameter_name.upcase
 
-    if required && ENV[parameter].nil?
-      abort("Must set #{parameter}")
-    end
+    abort("Must set #{parameter}") if required && ENV[parameter].nil?
 
     return false if ENV[parameter].nil?
 
@@ -124,70 +116,70 @@ class DroneSlack
 
   # Fetches the parameters set by the by Drone file
   def webhook
-    set_parameter("webhook")
+    set_parameter('webhook')
   end
 
   def channel
-    set_parameter("channel")
+    set_parameter('channel')
   end
 
   def recovery_mode
-    set_parameter("recovery_mode")
+    set_parameter('recovery_mode')
   end
 
   # These are environment variables set by Drone itself
   #
   # http://readme.drone.io/0.5/usage/environment-reference/
   def author
-    drone_env("commit_author")
+    drone_env('commit_author')
   end
 
   def branch
-    drone_env("commit_branch")
+    drone_env('commit_branch')
   end
 
   def build
-    drone_env("build_number")
+    drone_env('build_number')
   end
 
   def status
-    drone_env("job_status")
+    drone_env('job_status')
   end
 
   def started
-    drone_env("build_started")
+    drone_env('build_started')
   end
 
   def finished
-    drone_env("build_finished")
+    drone_env('build_finished')
   end
 
   def link
-    drone_env("build_link")
+    drone_env('build_link')
   end
 
   def repo_name
-    drone_env("repo")
+    drone_env('repo')
   end
 
   def repo_owner
-    drone_env("repo_owner")
+    drone_env('repo_owner')
   end
 
   def sha
-    drone_env("commit_sha")
+    drone_env('commit_sha')
   end
 
   def commit_message
-    drone_env("commit_message")
+    drone_env('commit_message')
   end
 
   def commit_link
-    drone_env("commit_link")
+    drone_env('commit_link')
   end
 
   def prev_build_status
-    drone_env("prev_build_status")
+    drone_env('prev_build_status')
   end
 end
 
